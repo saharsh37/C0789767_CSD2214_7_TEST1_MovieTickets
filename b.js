15b8907b8ded6bfd39ff4c3ff6e5b762b95d4bcb
saharsh37 <78936698+saharsh37@users.noreply.github.com>
2021-03-11 21:01:54 -0500

    Add files via upload

---
+++ b.js
@@ -0,0 +1,36 @@
+function get_movie() {
+
+      var e = document.getElementById("m_name").value;
+      document.getElementById('movie_name').innerHTML = '';
+      document.getElementById('movie_name').innerHTML = e;
+      if(e=="Dabbang")
+      {
+document.getElementById('price').innerHTML = '';
+      document.getElementById('price').innerHTML = "$10";
+  document.getElementById('no_seat').innerHTML = '';
+
+      }
+      else if(e=="Master")
+      {
+document.getElementById('price').innerHTML = '';
+      document.getElementById('price').innerHTML = "$12";
+  document.getElementById('no_seat').innerHTML = '';
+
+      }
+      else if(e=="Sultan")
+      {
+document.getElementById('price').innerHTML = '';
+      document.getElementById('price').innerHTML = "$8";
+  document.getElementById('no_seat').innerHTML = '';
+
+      }
+
+      else{
+            document.getElementById('price').innerHTML = '';
+      document.getElementById('price').innerHTML = "$9";
+  document.getElementById('no_seat').innerHTML = '';
+
+      }
+}
+             
+            
