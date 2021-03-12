2338247c9be88e0096110e678dc2b9b4595c87a8
saharsh37 <78936698+saharsh37@users.noreply.github.com>
2021-03-11 21:25:49 -0500

    Update b.js

--- b.js
+++ b.js
@@ -1,3 +1,43 @@
+$(document).ready(function() {
+  $('input[type="checkbox"]').click(function () {
+ var count=document.querySelectorAll("input:checked").length
+  document.getElementById('no_seat').innerHTML = '';
+  document.getElementById('no_seat').innerHTML = count;
+var e = document.getElementById("m_name").value;
+  
+  if(e=="Dabbang")
+  {
+
+p=count*10;
+document.getElementById('total').innerHTML = '';
+  document.getElementById('total').innerHTML = p;
+  }
+  else if(e=="Master")
+  {
+p=count*12;
+document.getElementById('total').innerHTML = '';
+  document.getElementById('total').innerHTML = p;
+
+  }
+  else if(e=="Sultan")
+  {
+p=count*8;
+document.getElementById('total').innerHTML = '';
+  document.getElementById('total').innerHTML = p;
+
+  }
+  else{
+p=count*9;
+document.getElementById('total').innerHTML = '';
+  document.getElementById('total').innerHTML = p;
+
+  } 
+
+});
+ 
+});
+
+
 function get_movie() {
 
       var e = document.getElementById("m_name").value;
@@ -32,5 +72,4 @@ document.getElementById('price').innerHTML = '';
 
       }
 }
-             
-            
+ 
